d094b1ad7d84a551c47dc424b0f0e1684d7c9101
Kaue Pereira <kaue@DESKTOP-E0B0O1A.localdomain>
2021-06-03 00:53:11 -0400

    Users list and some refactor from class to function components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,29 @@
-import { Component } from "react"
+import React from "react"
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-constructor() {
-    super();
-    this.state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
+const UserItem = ({user: { login, avatar_url, html_url }}) => {
+     
+    return (
+        <div className="card text-center">
+            <img 
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{ width:'60px' }}
+                />
+            <h3>{login}</h3>
+
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                    More
+                </a>
+            </div>
+        </div>
+    )
 }
 
-    render() {
-        return (
-            <div className="card text-center">
-                <img src={this.state.avatar_url} alt="" className="round-img" style={{ width:'60px' }}/>
-                <h3></h3>
-            </div>
-        )
-    }
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem
