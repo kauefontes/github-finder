9a703f5808edc1d4ab910985dfe59414aea31148
Kaue Pereira <kaue_fontes@hotmail.com>
2021-06-03 03:52:55 +0000

    Created Nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar/>
       </div>
     );
   }
