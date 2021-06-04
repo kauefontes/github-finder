9f91cae078117346559e29e8ece4403b20dc1209
Kaue Pereira <kaue@DESKTOP-E0B0O1A.localdomain>
2021-06-03 23:11:55 -0400

    Enabled search feature and executed linter
    Signed-off-by: Kaue Pereira <kaue@DESKTOP-E0B0O1A.localdomain>

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
   }
 
-  async componentDidMount() {
-    this.setState({ loading: true})
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
 
-    const res = await axios.get('https://api.github.com/users')
-    
-    this.setState({ users: res.data, loading: false})
-    console.log(res.data)
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    )
+
+    this.setState({ users: res.data.items, loading: false })
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false })
 
   render() {
+    const { users, loading } = this.state
+
     return (
       <div className="App">
-        <Navbar/>
-        <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
+        <Navbar />
+        <div className="container">
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={this.state.users} />
         </div>
       </div>
-    );
+    )
   }
 }
 
